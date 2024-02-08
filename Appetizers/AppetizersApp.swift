00b3b5ce3bb042e09cc1b01688a1fd4142538628
Yuri <yuriyuri200117@gmail.com>
2024-02-08 16:16:11 +0100

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Yuri Petrosyan on 08/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
